435b6d1b31a3c1de883ff42d1ba71a4d1fdc6a55
Rogerio de Souza <rogeriodesouza@MacBook-Air-de-Rogerio.local>
2020-06-08 01:54:35 -0300

    ST

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,13 @@
 import express from 'express';
-import routes from './routes';
-import cors from 'cors'
-import path from 'path'
+import cors from 'cors';
+import routes  from './routes';
+import path from 'path';
+import {errors} from 'celebrate';
 
 const app = express();
-app.use(express.json())
-app.use(routes)
 app.use(cors())
-app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
-app.listen(3333);
+app.use(express.json());
+app.use(routes);
+app.use('/uploads',express.static( path.resolve(__dirname,'..','uploads') ) );
+app.use(errors());
+app.listen(3333);   
